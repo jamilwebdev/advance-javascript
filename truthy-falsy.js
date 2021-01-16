55f30ea91e8cf84f87958aaf452ed82ddc3b68a4
ashrafphero <ashrafjamil.phero@gmail.com>
2021-01-16 21:15:52 +0600

    true va false

---
+++ truthy-falsy.js
@@ -0,0 +1,17 @@
+// falsy
+// 0
+// ""
+// undefined
+// null
+// NaN
+//truthy
+// '0' ' '  [], {}
+
+let names = 12;
+
+if(names == 0 || names){
+    console.log("is true");
+}
+else{
+    console.log("is false")
+}
