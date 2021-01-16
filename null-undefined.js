441272a2b9badb178565294341dc67a82400da22
ashrafphero <ashrafjamil.phero@gmail.com>
2021-01-16 23:00:28 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,12 @@
+// undefined
+
+// let pakhi;
+
+// function add(num1 , num2){
+ //   console.log(num1, num2);
+//    return;
+// }
+
+const student = {Name : "Jamil", Id : 5856956};
+console.log(student.phone);
+
